02eb057f19bb8904fa9dbbbf38d5633a1da17675
realbabilu <58899976+realbabilu@users.noreply.github.com>
2024-09-26 14:17:26 +0800

    PardisoDSS-ceanwang bandit-getenv-allocatable
    Cmakelists and run bat

---
+++ run.bat
@@ -0,0 +1,5 @@
+rmdir build /s
+mkdir build
+cd build
+cmake -G "Visual Studio 17 2022"  -D"CMAKE_FORTRAN_COMPILER=ifort.exe" -D"CMAKE_C_COMPILER=icx.exe" -D"CMAKE_CXX_COMPILER=icx.exe"  -D"TPL_ENABLE_BLAS=TRUE" -DBLA_VENDOR=Intel10_64lp -D"USE_XSDK_DEFAULTS_DEFAULT=TRUE" -D"XSDK_ENABLE_Fortran=TRUE" -D"CMAKE_BUILD_TYPE=RELEASE" -DMKLDSS=TRUE ..  
+
